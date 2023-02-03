22d9c2057c0d3baddc7e01924230814f8950ae97
OdeToTheCode <119718185+OdeToTheCode@users.noreply.github.com>
2023-02-03 00:03:29 -0600

    finished katies code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,18 +4,38 @@ const { v4 } = require('uuid')
 
 // GET "/api/notes" responds with all notes from the database
 router.get('/notes', (req, res) => {
-  
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    err && res.status(500)
+    res.json(JSON.parse(data))
+  })
 });
 
 // POST "/api/notes" adds a note to the database
 router.post('/notes', (req, res) => {
-  
+  const notes = {...req.body, id: v4()}
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    err && res.status(500)
+    const parsed = JSON.parse(data)
+    parsed.push(notes)
+    fs.writeFile('./db/db.json', JSON.stringify(parsed), err => {
+      err && res.status(500)
+      res.json(parsed)
+    })
+  })
 });
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
 router.delete('/notes/:id', (req, res) => {
-  
-});
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    err && res.status(500)
+    const parsed = JSON.parse(data)
+    const notes = parsed.filter(dude => dude.id !== req.params.id)
+    fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+      err && res.status(500)
+      res.json(notes)
+    })
+  })
+})
 
 // PUT "api/notes" updates a note with an id equal to req.params.id
 router.put('/notes/:id', (req, res) => {
--- server.js
+++ server.js
@@ -13,15 +13,5 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-})
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__direname, '/public/notes.html'))
-})
-
 // Start the server on the port
-app.listen(PORT, () => 
-  console.log(`Listening at http://localhost:${PORT} 🚀`)
-  );
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
